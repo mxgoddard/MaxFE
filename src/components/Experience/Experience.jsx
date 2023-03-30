f436f2ab77fb7b7238e253e06263d0bab02feaa5
mxgoddard <maxolivergoddard@gmail.com>
2023-03-30 15:56:35 +0100

    Deployment fixes

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -7,11 +7,6 @@ import TimelineContent from '@mui/lab/TimelineContent';
 import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
 import TimelineDot from '@mui/lab/TimelineDot';
 import Typography from '@mui/material/Typography';
-import BallyInteractiveLogo from '../../images/bally-interactive-logo.jpg';
-import Degree53Logo from '../../images/degree53-logo.png';
-import AccessPayLogo from '../../images/accesspay-logo.jpg';
-import NorthcodersLogo from '../../images/northcoders-logo.jpg';
-import TanglinTrustSchoolLogo from '../../images/tts-logo.jpg';
 import './Experience.css';
 import { Paper } from '@mui/material';
 // import GeneralTestApiCall from '../../helpers/NetworkHelper.js;'
@@ -25,12 +20,12 @@ export default class Experience extends React.Component {
 
     state = {
         experience: [
-            { company: 'Bally Interactive', role: 'Full Stack Developer', startDate: 'November 2021', logo: BallyInteractiveLogo },
-            { company: 'Degree53', role: 'Full Stack Developer', startDate: 'July 2020', logo: Degree53Logo },
-            { company: 'AccessPay', role: 'Associate Software Developer', startDate: 'August 2019', logo: AccessPayLogo },
-            { company: 'AccessPay', role: 'Junior Software Engineer', startDate: 'January 2019', logo: AccessPayLogo },
-            { company: 'Northcoders', role: 'Trainee Stack Developer', startDate: 'September 2018', logo: NorthcodersLogo },
-            { company: 'Tanglin Trust School', role: 'Computer Science, Business, Maths', startDate: '', logo: TanglinTrustSchoolLogo }
+            { company: 'Bally Interactive', role: 'Full Stack Developer', startDate: 'November 2021' },
+            { company: 'Degree53', role: 'Full Stack Developer', startDate: 'July 2020' },
+            { company: 'AccessPay', role: 'Associate Software Developer', startDate: 'August 2019' },
+            { company: 'AccessPay', role: 'Junior Software Engineer', startDate: 'January 2019' },
+            { company: 'Northcoders', role: 'Trainee Stack Developer', startDate: 'September 2018' },
+            { company: 'Tanglin Trust School', role: 'Computer Science, Business, Maths', startDate: '' }
         ]
     }
 
@@ -53,10 +48,6 @@ export default class Experience extends React.Component {
                                 <TimelineSeparator>
                                     <TimelineConnector />
                                     <TimelineDot sx={{ padding: '0' }}>
-                                        {/* <LaptopMacIcon /> */}
-                                        <Paper className='ExperienceCompanyLogoContainer' sx={{ borderRadius: '50%' }}>
-                                            <img src={data.logo} alt='logo' className='ExperienceCompanyLogo' />
-                                        </Paper>
 
                                     </TimelineDot>
                                     <TimelineConnector />
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,7 @@
 import { Typography, Button } from '@mui/material';
 import React from 'react';
+import Experience from '../Experience/Experience';
+import Hero from './Hero/Hero';
 import './Home.css';
 
 function Home() {
@@ -7,32 +9,8 @@ function Home() {
 
     return (
         <div className='Home'>
-            <video width="750" height="500" autoPlay muted loop>
-                <source src={require('../../videos/HeroVideo.mp4')} type="video/mp4" />
-                Your browser does not support the video tag. I suggest you upgrade your browser.
-            </video>
-            <div className='Home-Container'>
-                <Typography variant="h6"
-                    component="div"
-                    id='Home-Whoami'
-                    sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}>// Who Am I</Typography>
-
-                <Typography variant="h6"
-                    component="div"
-                    id='Home-Job-Title'
-                    sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block', maxWidth: '50vw', lineHeight: '1', marginTop: '2rem' } }}>Full-Stack Software Developer</Typography>
-
-                <Typography variant="h6"
-                    component="div"
-                    id='Home-About-Description'
-                    sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block', maxWidth: '50vw', marginTop: '2rem' } }}>
-                    As a software developer with 5 years of experience, I am passionate about using my skills to create innovative solutions that solve complex problems.
-                </Typography>
-
-                <Button key={'Hero-Get-In-Touch'} id='Nav-Bar-Link-Login' sx={{ backgroundColor: '#141414', borderRadius: '100px', padding: '1rem 2rem', marginTop: '2rem' }}>
-                    {'Get in touch →'}
-                </Button>
-            </div>
+            <Hero />
+            {/* <Experience /> */}
         </div>
     );
 };
